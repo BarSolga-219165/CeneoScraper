6c2166f8f4bebc7c9e5ff93c09b466d2174d0961
BarSolga-219165 <s219165@student.uek.krakow.pl>
2021-05-30 11:01:21 +0200

    2021-05-30 BS

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # CeneoScraper
 ## Etap 1 - Pobranie składowych pojedynczej opinii o wybranym produkcie [Ceneo.pl](https://www.ceneo.pl/)
-1.Pobranie kodu pojedynczej strony z opiniami o produkcie 
+* Pobranie kodu pojedynczej strony z opiniami o produkcie
+* Analiza kodu html pojedynczej opinii
+
++---------------+------------+--------------+----------+
+|Składowa opinii|Selektor CSS|Nazwa zmiennej|Typ danych|
++===============+============+==============+==========+
+|Opinia|.js_product-review|opinion|||
++---------------+------------+--------------+----------+
+|Identyfikator opinii|['data-review-id']|opinion_id|||
++---------------+------------+--------------+----------+
+|Autor|.user-post__author-name|author|||
++---------------+------------+--------------+----------+
+|Rekomendacje|.user-post__author-recomendation|recomm|||
++---------------+------------+--------------+----------+
+|Liczba gwiazdek|.user-post__score-count|stars|||
++---------------+------------+--------------+----------+
+|Treść|.user-post__text|content|||
++---------------+------------+--------------+----------+
+|Lista zalet|review-feature__col:has(> div.review-feature__title--positives) > .review-feature__item|pros|||
+||review-feature__col:has(> div[class*="positives") > .review-feature__item|||
+||div.review-feature__title--positives ~ .review-feature__item|||
++---------------+------------+--------------+----------+
+|Lista wad|review-feature__col:has(> div.review-feature__title--negatives) > .review-feature__item|cons|||
+||review-feature__col:has(> div[class*="negatives") > .review-feature__item|||
+||div.review-feature__title--negatives ~ .review-feature__item|||
++---------------+------------+--------------+----------+
+|Dla ilu osób użyteczna|span[id^="votes-yes"]|useful|||
++---------------+------------+--------------+----------+
+|Dla ilu osób nieużyteczna|span[id^="votes-no"]|useless|||
++---------------+------------+--------------+----------+
+|Czy opinia potwierdzona zakupem|.review-pz|purchased|||
++---------------+------------+--------------+----------+
+|Data wystawienia opinii|.user-post__published > time:nth-child(1)`["datetime"]`|publish_date|||
++---------------+------------+--------------+----------+
+|Data zakupu|.user-post__published > time:nth-child(2)`["datetime"]`|purchase_date|||
++---------------+------------+--------------+----------+
--- scraper.py
+++ scraper.py
@@ -1,5 +1,71 @@
+from os import write
 import requests as req
+from bs4 import BeautifulSoup
+import json
 
-respons = req.get("https://www.ceneo.pl/55892514#tab=reviews")
+def get_feature(dom_tree, selector, attribute=None):
+    try:
+        if isinstance(attribute,str):
+            return dom_tree.select_one(selector)[attribute].strip()
+        if isinstance(attribute, list):
+            return [element.text.strip() for element in dom_tree.select(selector)]
+        return dom_tree.select_one(selector).text.strip()
+    except (AttributeError, TypeError):
+        return None
 
-print(respons.text)
+next_page = "https://www.ceneo.pl/55892514"
+all_opinions = []
+
+while next_page:
+    respons = req.get("next_page")
+
+    # print(respons.text) #.encode('cp1252', errors='ignore')
+
+    page_dom = BeautifulSoup(respons.text, "html.parser")
+
+    opinions = page_dom.select("div.js_product-review")
+    # print(type(opinions))
+    opinion = opinions.pop(0)
+    # print(type(opinion))
+
+    all_opinions = []
+
+    for opinion in opinions:
+        single_opinion = {
+            'opinion_id': opinion["data-entry-id"],
+                'author': get_feature(opinion, "span.user-post__author-name"),
+                'recomm': get_feature(opinion,"span.user-post__author-recomendation"),
+                'stars': get_feature(opinion,"span.user-post__score-count"),
+                'content': get_feature(opinion,"div.user-post__text"),
+                'pros': get_feature(opinion, "div.review-feature__title--positives ~ .review-feature__item", []),
+                'cons': get_feature(opinion, "div.review-feature__title--negatives ~ .review-feature__item", []),
+                'useful': get_feature(opinion, "button.vote-yes > span"),
+                'useless': get_feature(opinion, "button.vote-no > span"),
+                'purchased': get_feature(opinion, "div.review-pz"),
+                'publish_date': get_feature(opinion, "span.user-post__published > time:nth-child(1)", "datetime"),
+                'purchase_date': get_feature(opinion,"span.user-post__published > time:nth-child(2)", "datetime")
+        }
+        all_opinions.append(single_opinion)
+
+try:
+    next_page = "https://www.ceneo.pl/" + get_feature(page_dom, "pagination__item pagination__next", "href")
+except TypeError:
+    next_page = None
+
+with open("opinions/55892514.json", "w" ,encoding="UTF-8") as jf:
+    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+
+
+# json.dumps(all_opinions, indent=4, ensure_ascii=False)
+
+
+
+# print(opinion_id, author, recomm, stars, content, pros, cons, useful, useless, purchased, publish_date, purchase_date)
+
+## pip freeze > requirements.txt
+#git add .
+# git commit -m "2021-05-08 BS"
+
+
+# print(page_dom.prettify())
+
